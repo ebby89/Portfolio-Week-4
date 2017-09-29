ed5800b68edebeaa6604fc7f2e73052c2cc5ee12
Kyle Ebersole <kyle.m.ebersole@gmail.com>
2017-09-29 14:39:33 -0700

    Remove topping selector. Add checkboxes to replace topping selector. Add function to add another pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -14,23 +14,43 @@ function Topping() {
 //FRONT END
 $(document).ready(function(){
 
-  $("#add-topping").click(function(){
-    $("#new-topping").append('<div id="new-topping">' +
-                               '<h4><strong>Select a topping:</strong></h4>' +
-                               '<select class="" name="">' +
-                                 '<option value="">Select a topping...</option>' +
-                                 '<option value="">Pepperoni</option>' +
-                                 '<option value="">Sausage</option>' +
-                                 '<option value="">Chicken</option>' +
-                                 '<option value="">Bell Pepper</option>' +
-                                 '<option value="">Caramalized Onion</option>' +
-                                 '<option value="">Mushroom</option>' +
-                                 '<option value="">Olive</option>' +
-                                 '<option value="">Arugula</option>' +
-                                 '<option value="">Spinnach</option>' +
-                                 '<option value="">Tomato</option>' +
-                               '</select>' +
-                             '</div>');
+  $("#add-pizza").click(function(){
+    $("#new-pizza").append('<div id="new-pizza" class="">' +
+                             '<h3>Your Order:</h3>' +
+                             '<h4><strong>Select a size:</strong></h4>' +
+                             '<select class="" name="">' +
+                               '<option value="">Select a size...</option>' +
+                               '<option value="">Small (10")</option>' +
+                               '<option value="">Medium (14")</option>' +
+                               '<option value="">Large (18")</option>' +
+                               '<option value="">Extra-Large (22")</option>' +
+                             '</select> Add details here' +
+                             '<br>' +
+                             '<br>' +
+                             '<h4><strong>Select your sauce:</strong></h4>' +
+                             '<select class="" name="">' +
+                               '<option value="">Select a sauce...</option>' +
+                               '<option value="">Traditional Marinara</option>' +
+                               '<option value="">Creamy Garlic Parmesan</option>' +
+                               '<option value="">Barbeque</option>' +
+                               '<option value="">Pesto</option>' +
+                             '</select> Add details here' +
+                             '<br>' +
+                             '<br>' +
+                             '<div class="">' +
+                               '<h4><strong>Select your toppings:</strong></h4>' +
+                               '<input type="checkbox" name="toppings" value="pepperoni"> Pepperoni<br>' +
+                               '<input type="checkbox" name="toppings" value="sausage"> Italian Sausage<br>' +
+                               '<input type="checkbox" name="toppings" value="chicken"> Chicken<br>' +
+                               '<input type="checkbox" name="toppings" value="peppers"> Bell Peppers<br>' +
+                               '<input type="checkbox" name="toppings" value="onion"> Caramalized Onions<br>' +
+                               '<input type="checkbox" name="toppings" value="mushroom"> Mushrooms<br>' +
+                               '<input type="checkbox" name="toppings" value="olive"> Olives<br>' +
+                               '<input type="checkbox" name="toppings" value="tomato"> Tomato<br>' +
+                               '<input type="checkbox" name="toppings" value="spinnach"> Spinnach<br>' +
+                             '</div>' +
+                           '</div>');
+
 
   });
 
