d10402a35d6225a9499db68ad8015712c7777321
Kyle Ebersole <kyle.m.ebersole@gmail.com>
2017-09-29 14:19:19 -0700

    Add function to add multiple toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
 //BACK END
+function Pizza(size, sauce, toppings) {
+  this.size = size;
+  this.sauce = sauce;
+  this.toppings = [];
+}
 
+function Topping() {
+
+}
 
 
 
 //FRONT END
 $(document).ready(function(){
-  $("#").submit(function(event){
-  event.preventDefault();
+
+  $("#add-topping").click(function(){
+    $("#new-topping").append('<div id="new-topping">' +
+                               '<h4><strong>Select a topping:</strong></h4>' +
+                               '<select class="" name="">' +
+                                 '<option value="">Select a topping...</option>' +
+                                 '<option value="">Pepperoni</option>' +
+                                 '<option value="">Sausage</option>' +
+                                 '<option value="">Chicken</option>' +
+                                 '<option value="">Bell Pepper</option>' +
+                                 '<option value="">Caramalized Onion</option>' +
+                                 '<option value="">Mushroom</option>' +
+                                 '<option value="">Olive</option>' +
+                                 '<option value="">Arugula</option>' +
+                                 '<option value="">Spinnach</option>' +
+                                 '<option value="">Tomato</option>' +
+                               '</select>' +
+                             '</div>');
+
+  });
+
+  $("#order-form").submit(function(event){
+    event.preventDefault();
 
   });
 });
