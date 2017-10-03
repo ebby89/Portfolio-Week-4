68f3fe1840eded596528d07aed6ead4e417cd03c
Kyle Ebersole <kyle.m.ebersole@gmail.com>
2017-10-03 12:53:46 -0700

    Remove add pizza button. Add form reset functionality. Add alert for unselected size.

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,17 @@ function Pizza(size) {
 Pizza.prototype.totalPrice = function() {
   var price = 0;
 
-  if (this.size === "S") {
+  if (this.size === "Small") {
     price += 10;
-  } else if (this.size === "M") {
+  } else if (this.size === "Medium") {
     price += 12;
-  } else if (this.size === "L") {
+  } else if (this.size === "Large") {
     price += 14;
-  } else {
+  } else if (this.size === "Extra-Large") {
     price += 16;
+  } else {
+    price = 0;
+    alert("Please select a size");
   }
 
   if (this.toppings.length === 0) {
@@ -28,37 +31,12 @@ Pizza.prototype.totalPrice = function() {
   return total;
 };
 
+function resetForm() {
+ document.getElementById("order-form").reset();
+}
 
 //FRONT END
 $(document).ready(function(){
-  $("#add-pizza").click(function(){
-    $("#new-pizza").append('<div id="new-pizza" class="">' +
-                             '<h3>Your Order:</h3>' +
-                             '<h4><strong>Select a size:</strong></h4>' +
-                             '<select class="" name="">' +
-                               '<option value="">Select a size...</option>' +
-                               '<option value="">Small (10")</option>' +
-                               '<option value="">Medium (14")</option>' +
-                               '<option value="">Large (18")</option>' +
-                               '<option value="">Extra-Large (22")</option>' +
-                             '</select>' +
-                             '<br>' +
-                             '<br>' +
-                             '<div class="">' +
-                               '<h4><strong>Select your toppings:</strong></h4>' +
-                               '<input type="checkbox" name="toppings" value="pepperoni"> Pepperoni<br>' +
-                               '<input type="checkbox" name="toppings" value="sausage"> Italian Sausage<br>' +
-                               '<input type="checkbox" name="toppings" value="chicken"> Chicken<br>' +
-                               '<input type="checkbox" name="toppings" value="peppers"> Bell Peppers<br>' +
-                               '<input type="checkbox" name="toppings" value="onion"> Caramalized Onions<br>' +
-                               '<input type="checkbox" name="toppings" value="mushroom"> Mushrooms<br>' +
-                               '<input type="checkbox" name="toppings" value="olive"> Olives<br>' +
-                               '<input type="checkbox" name="toppings" value="tomato"> Tomato<br>' +
-                               '<input type="checkbox" name="toppings" value="spinnach"> Spinnach<br>' +
-                             '</div>' +
-                           '</div>');
-  });
-
   $("form#order-form").submit(function(event){
     event.preventDefault();
 
@@ -70,11 +48,13 @@ $(document).ready(function(){
     });
 
     var displayPrice = newPizza.totalPrice();
-    console.log(displayPrice);
     $("#display-size").text(pizzaSizeInput);
     $("#display-toppings").text(newPizza.toppings);
+
     $("#display-cost").text(displayPrice);
 
     $("#display-order-summary").fadeIn(600);
+
+    resetForm();
   });
 });
